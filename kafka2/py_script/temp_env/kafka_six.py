c5ec9517622c46c64852a705128fa784e4f46608
mohamad <m.a.fakhrian.7744@gmail.com>
2024-12-11 10:47:46 +0330

    clean

--- kafka2/py_script/temp_env/kafka_six.py
+++
@@ -1,5 +0,0 @@
-try:
-    import six
-    print("Six package is working")
-except ImportError as e:
-    print(f"Error importing Six package: {e}")
--- kafka2/py_script/test_kafka.py
+++
@@ -1,7 +0,0 @@
-try:
-    from kafka import KafkaProducer, KafkaConsumer
-    from kafka.admin import KafkaAdminClient, NewTopic
-    print("Kafka package is working")
-except ImportError as e:
-    print(f"Error importing Kafka package: {e}")
-
